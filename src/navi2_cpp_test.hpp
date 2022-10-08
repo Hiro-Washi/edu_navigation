ead826c4f73cc3d38f23f1b71d0d283dd2d80508
Hiroto <c1115332@planet.kanazawa-it.ac.jp>
2022-10-08 16:58:23 +0900

    1008;1806: dell(pull->add->commit)

---
+++ src/navi2_cpp_test.hpp
@@ -0,0 +1,21 @@
+# FoxyのNavi2でC++を書く前にNoeticでC++Naviの練習・・・
+#include <ros/ros.h>
+#include <actionlib>
+#include <std_msgs/Sting>
+#include <std_msgs/Float64>
+#include <std_srvs/Empty>
+#include <edu_navigation/locationName>
+#include <edu_navigation/Navi2>
+#include <move_base_msgs/MoveBaseAction>
+#include <move_base_msgs/MoveBaseGoal>
+//#include "rosparam"
+
+class Navi2Server{
+private:
+    ros::NodeHandle nh;
+    ros::ser
+    ros::
+
+public:
+    
+    
